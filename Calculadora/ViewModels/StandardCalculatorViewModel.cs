4dfb2a61bc27a085d9878c9028eb02ea4338cb0d
FastShowDev <zlFastShow@hotmail.com>
2022-11-28 16:20:33 -0300

    Create initial StandardCalculatorViewModel

---
+++ Calculadora/ViewModels/StandardCalculatorViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Calculadora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.ViewModels
+{
+    public class StandardCalculatorViewModel : BaseViewModel
+    {
+        private Calculator calculator;
+
+        #region Properties
+        private string _displayContent = "0";
+        public string DisplayContent
+        {
+            get { return _displayContent; }
+            set { _displayContent = value; }
+        }
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        public StandardCalculatorViewModel()
+        {
+            calculator = new Calculator();
+        }
+
+
+    }
+}
