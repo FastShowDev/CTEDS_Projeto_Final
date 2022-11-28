cee94acb09386e090754049e1187b8b423ea6422
FastShowDev <zlFastShow@hotmail.com>
2022-11-28 17:00:12 -0300

    Create Command that close the Menu

---
+++ Calculadora/Commands/CloseMenuCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Calculadora.Commands
+{
+    public class CloseMenuCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public CloseMenuCommand()
+        {
+
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter != null)
+            {
+                Border menu = (Border)parameter;
+                menu.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
