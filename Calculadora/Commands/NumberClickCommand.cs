f404f2a47328c7e6638c9c4aa68dd4ce5eb75a3e
FastShowDev <zlFastShow@hotmail.com>
2022-11-28 17:01:18 -0300

    Create Command that update the display when a number button is pressed

---
+++ Calculadora/Commands/NumberClickCommand.cs
@@ -0,0 +1,46 @@
+﻿using Calculadora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Calculadora.Commands
+{
+    public class NumberClickCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Calculator _calculator;
+
+        public NumberClickCommand(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _calculator.isNumber = true;
+            string objName = ((Button)parameter).Name;
+            string pressedButtonValue = Convert.ToString((parameter as Button).Content);
+            if (_calculator.displayContent == "0" && pressedButtonValue != _calculator.DECIMAL_SPERATOR)
+            {
+                _calculator.displayContent = "";
+            }
+            if (objName == "button_float")
+            {
+                _calculator.isFloat = true;
+            }
+            _calculator.displayContent += pressedButtonValue;
+            _calculator.lastButtonPressed = "number";
+        }
+    }
+}
