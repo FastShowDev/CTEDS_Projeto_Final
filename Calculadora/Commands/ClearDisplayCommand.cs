1ebd787873c8cae7583bcd294e130e2168282a75
FastShowDev <zlFastShow@hotmail.com>
2022-11-29 12:31:17 -0300

    Create Command for C and CE Buttons

---
+++ Calculadora/Commands/ClearDisplayCommand.cs
@@ -0,0 +1,21 @@
+﻿using Calculadora.ViewModels;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class ClearDisplayCommand : CommandBase
+    {
+        private readonly StandardCalculatorViewModel _viewModel;
+        public ClearDisplayCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _viewModel.displayContent = "0";
+            _viewModel.stringResult = "";
+            _viewModel.calculator.lastButtonPressed = "number";
+        }
+    }
+}
