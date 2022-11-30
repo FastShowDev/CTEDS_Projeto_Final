e21b1f1cebf6e3ce345e0599557737226c2d1ea5
FastShowDev <zlFastShow@hotmail.com>
2022-11-30 17:49:08 -0300

    Create DbContext and add dependencies

---
+++ Calculadora/Context/Context.cs
@@ -0,0 +1,41 @@
+﻿using Calculadora.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<History> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<History>(entity =>
+            {
+                entity.HasData(
+                    new History
+                    {
+                        Id = new Guid(),
+                        expression = "18+4-2+5",
+                        result = "25",
+                    },
+                    new History
+                    {
+                        Id = new Guid(),
+                        expression = "",
+                        result = "",
+                    });
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
