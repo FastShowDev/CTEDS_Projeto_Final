d4896a9ae0715b4202f741c0783d580504256f7b
FastShowDev <zlFastShow@hotmail.com>
2022-11-30 17:48:48 -0300

    Add new Models for refactoring

---
+++ Calculadora/Models/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Models
+{
+    public class History
+    {
+        public Guid Id { get; set; }
+        public string? expression { get; set; }
+        public string? result { get; set; }
+    }
+}
