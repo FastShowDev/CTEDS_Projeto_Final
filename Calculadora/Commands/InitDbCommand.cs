973b88de0b5abbbbb8c956286e288bcb0a8c0482
FastShowDev <zlFastShow@hotmail.com>
2022-11-30 21:47:20 -0300

    Create a command that inicialize the datagrid

---
+++ Calculadora/Commands/InitDbCommand.cs
@@ -0,0 +1,27 @@
+﻿using Calculadora.Database;
+using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class InitDbCommand : CommandBase
+    {
+        private Context test;
+        private StandardCalculatorViewModel _viewModel;
+        public InitDbCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            DataGrid dataGrid = (parameter as DataGrid);
+            dataGrid.DataContext = _viewModel.context;
+            dataGrid.ItemsSource = _viewModel.histories;
+        }
+    }
+}
