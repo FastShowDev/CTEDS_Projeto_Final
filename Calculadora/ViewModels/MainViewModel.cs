1e9928d6bc477e1123adfd50bbb1224933f18200
FastShowDev <zlFastShow@hotmail.com>
2022-12-03 13:32:47 -0300

    Create ViewModel for Main View

---
+++ Calculadora/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Calculadora.Stores;
+using System.ComponentModel;
+
+namespace Calculadora.ViewModels
+{
+    public class MainViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        public BaseViewModel CurrentViewModel => NavigationStore.CurrentViewModel;
+
+        public MainViewModel()
+        {
+            NavigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+            ViewName = "Main";
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+
+    }
+}
