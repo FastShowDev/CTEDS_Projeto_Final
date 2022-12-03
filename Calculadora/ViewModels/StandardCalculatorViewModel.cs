03e095d3db2d18b25073686d8b8234959fdb533e
FastShowDev <zlFastShow@hotmail.com>
2022-12-03 13:36:15 -0300

    Update BaseViewModel and StandardCalculatorViewModel for the new navigation system

--- Calculadora/ViewModels/StandardCalculatorViewModel.cs
+++ Calculadora/ViewModels/StandardCalculatorViewModel.cs
@@ -1,22 +1,20 @@
 ﻿using Calculadora.Commands;
 using Calculadora.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using Calculadora.Database;
 using Microsoft.EntityFrameworkCore;
+using Calculadora.Stores;
+using System.ComponentModel;
 
 namespace Calculadora.ViewModels
 {
-    public class StandardCalculatorViewModel : BaseViewModel
+    public class StandardCalculatorViewModel : BaseViewModel, INotifyPropertyChanged
     {
         public Calculator calculator;
         public readonly Context context;
         public List<History> histories;
-
 
         #region Properties
         private string _displayContent = "0";
@@ -32,36 +30,46 @@ namespace Calculadora.ViewModels
             get { return _stringResult; }
             set { _stringResult = value; OnPropertyChanged(nameof(stringResult)); }
         }
+
+
+
+
         #endregion
 
 
         #region Commands
+        //public new ICommand OpenMenu { get; }
+        //public new ICommand CloseMenu { get; }
+        //public ICommand NavigateMenuCM { get; }
         public ICommand BackspaceCM { get; }
         public ICommand ClearDisplayCM { get; }
         public ICommand NumberClickCM { get; }
         public ICommand OperatorClickCM { get; }
         public ICommand CalculateCM { get; }
-        public ICommand InitDbCM { get; }
+        public ICommand LoadHistoryCM { get; }
         #endregion
 
 
         public StandardCalculatorViewModel()
         {
             calculator = new Calculator();
+            //CloseMenu = new CloseMenuCommand(this);
+            //OpenMenu = new OpenMenuCommand(this);
+            //NavigateMenuCM = new NavigateCommand();
             BackspaceCM = new BackspaceCommand(this);
             ClearDisplayCM = new ClearDisplayCommand(this);
             NumberClickCM = new NumberClickCommand(this);
             OperatorClickCM = new OperatorClickCommand(this);
             CalculateCM = new CalculateCommand(this);
-            InitDbCM = new InitDbCommand(this);
+            LoadHistoryCM = new LoadHistoryCommand(this);
 
-            var contextOptions = new DbContextOptionsBuilder<Context>().UseSqlite("Data source = History.db").Options;
+            var contextOptions = new DbContextOptionsBuilder<Context>().UseSqlite("Data source = Histories.db").Options;
             context = new Context(contextOptions);
 
-            histories = context.History.ToList();
+            histories = context.Histories.ToList();
 
+            ViewName = "Padrão";
         }
-
 
     }
 }
