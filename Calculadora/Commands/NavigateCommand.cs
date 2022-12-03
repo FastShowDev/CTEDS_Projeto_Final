d58cc42276f47a645265e799ee0b62023b68e4f2
FastShowDev <zlFastShow@hotmail.com>
2022-12-03 13:31:33 -0300

    Create initial base for Navigate Command

---
+++ Calculadora/Commands/NavigateCommand.cs
@@ -0,0 +1,23 @@
+﻿using Calculadora.Stores;
+using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculadora.Commands
+{
+    public class NavigateCommand : CommandBase
+    {
+        public NavigateCommand()
+        {
+        }
+
+        public override void Execute(object? parameter)
+        {
+            NavigationStore.CurrentViewModel = new ScientificCalculatorViewModel();
+        }
+    }
+}
