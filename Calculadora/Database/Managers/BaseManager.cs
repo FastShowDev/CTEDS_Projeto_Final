75b424ec6d4324444874def0c4e6d9e12672e744
FastShowDev <zlFastShow@hotmail.com>
2022-12-07 14:19:09 -0300

    Create Base Manager and HistoryManager

---
+++ Calculadora/Database/Managers/BaseManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Calculadora.Database.Managers
+{
+    public class BaseManager
+    {
+        public CalculatorDbContext context { get; }
+        public BaseManager(CalculatorDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ Calculadora/Database/Managers/HistoryManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculadora.Database.Managers
+{
+    public class HistoryManager : BaseManager
+    {
+        public HistoryManager(CalculatorDbContext context) : base(context)
+        {
+
+        }
+    }
+}
