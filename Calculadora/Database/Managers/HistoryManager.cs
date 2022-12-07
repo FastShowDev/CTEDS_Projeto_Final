1c7415558ccb61580560b6503c3edfaf0a185b78
FastShowDev <zlFastShow@hotmail.com>
2022-12-07 19:04:15 -0300

    Create database manager for history

--- Calculadora/Database/Managers/HistoryManager.cs
+++ Calculadora/Database/Managers/HistoryManager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Calculadora.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Calculadora.Database.Managers
 {
@@ -7,6 +11,25 @@ namespace Calculadora.Database.Managers
         public HistoryManager(CalculatorDbContext context) : base(context)
         {
 
+        }
+
+        public async Task<List<History>> GetAllHistories()
+        {
+            List<History> histories = await context.Histories.ToListAsync();
+            return histories;
+        }
+        
+
+        public async void AddHistory(History history)
+        {
+            context.Histories.Add(history);
+            await context.SaveChangesAsync();
+        }
+
+        public async void DeleteHistory(History history)
+        {
+            context.Histories.Remove(history);
+            await context.SaveChangesAsync();
         }
     }
 }
