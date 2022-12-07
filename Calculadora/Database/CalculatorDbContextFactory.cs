ebc1db8f1896e36825e80680ab996a87e699a15e
FastShowDev <zlFastShow@hotmail.com>
2022-12-07 14:15:48 -0300

    Create Auxiliar Class for create dbContext

---
+++ Calculadora/Database/CalculatorDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Calculadora.Database
+{
+    public class CalculatorDbContextFactory
+    {
+        private readonly string _connectionString;
+
+        public CalculatorDbContextFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public CalculatorDbContext CreateDbContext()
+        {
+            DbContextOptions<CalculatorDbContext> options = new DbContextOptionsBuilder<CalculatorDbContext>().UseSqlite(_connectionString).Options;
+            return new CalculatorDbContext(options);
+        }
+
+
+    }
+}
