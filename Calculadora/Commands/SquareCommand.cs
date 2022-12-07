d47eaa38ec940ea78c2355bb6b58a274c847e495
FastShowDev <zlFastShow@hotmail.com>
2022-12-07 14:08:38 -0300

    Create Square and SquareRoot commands

---
+++ Calculadora/Commands/SquareCommand.cs
@@ -0,0 +1,30 @@
+﻿using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Commands
+{
+    public class SquareCommand : BaseCommand
+    {
+        private readonly StandardCalculatorViewModel _viewModel;
+
+        public SquareCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                string expression = parameter as string;
+                _viewModel.calculator.InsertSquareInDisplay(expression);
+                _viewModel.UpdateDisplay();
+            }
+
+        }
+    }
+}
---
+++ Calculadora/Commands/SquareRootCommand.cs
@@ -0,0 +1,38 @@
+﻿using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class SquareRootCommand : BaseCommand
+    {
+        private readonly StandardCalculatorViewModel _viewModel;
+        public SquareRootCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                string expression = ((TextBox)parameter).Text;
+                _viewModel.calculator.InsertSquareRootInDisplay(expression);
+                _viewModel.displayContent = _viewModel.calculator.displayContent;
+                _viewModel.stringResult = _viewModel.calculator.result;
+                MessageBox.Show("Resultado: " + _viewModel.calculator.result + "\nDisplay: " + _viewModel.calculator.displayContent);
+                _viewModel.calculator.hasCalculate = true;
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+
+        }
+    }
+}
