09f98c8f4eb0c6c7e2a1ebb1d87008b228d45734
FastShowDev <zlFastShow@hotmail.com>
2022-12-08 11:37:52 -0300

    Create a command for open history menu and update data grid

---
+++ Calculadora/Commands/OpenHistoryCommand.cs
@@ -0,0 +1,37 @@
+﻿using Calculadora.Stores;
+using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Calculadora.Commands
+{
+    public class OpenHistoryCommand : BaseCommand
+    {
+        StandardCalculatorViewModel _viewModel;
+        public OpenHistoryCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+#nullable enable
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+#nullable enable
+        public override void Execute(object? parameter)
+        {
+            var values = (object[])parameter;
+            Border historyMenu = values[0] as Border;
+            DataGrid dataGrid = values[1] as DataGrid;
+
+            historyMenu.Visibility = Visibility.Visible;
+            dataGrid.ItemsSource = _viewModel.GetAllHistories();
+            NavigationStore.CurrentViewModel.IsMenuOpen = true;
+        }
+    }
+}
