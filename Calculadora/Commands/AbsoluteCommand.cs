e729e6d93a3e7428453f963411e3d0fb41172fdc
mtlira <matheusribeiro10@gmail.com>
2022-12-07 21:29:55 -0300

    implementação das funcionalidades de abs, log, ln e inverso

---
+++ Calculadora/Commands/AbsoluteCommand.cs
@@ -0,0 +1,39 @@
+﻿using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class AbsoluteCommand : BaseCommand
+    {
+        private readonly StandardCalculatorViewModel _viewModel;
+        public AbsoluteCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                string expression = ((TextBox)parameter).Text;
+                _viewModel.calculator.InsertAbsoluteInDisplay(expression);
+                _viewModel.displayContent = _viewModel.calculator.displayContent;
+                _viewModel.stringResult = _viewModel.calculator.result;
+                //MessageBox.Show("Resultado: " + _viewModel.calculator.result + "\nDisplay: " + _viewModel.calculator.displayContent);
+                _viewModel.calculator.hasCalculate = true;
+                _viewModel.AddHistory(_viewModel.stringResult, _viewModel.displayContent);
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+
+        }
+    }
+}
---
+++ Calculadora/Commands/LnCommand.cs
@@ -0,0 +1,39 @@
+﻿using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class LnCommand : BaseCommand
+    {
+        private readonly StandardCalculatorViewModel _viewModel;
+        public LnCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                string expression = ((TextBox)parameter).Text;
+                _viewModel.calculator.InsertLnInDisplay(expression);
+                _viewModel.displayContent = _viewModel.calculator.displayContent;
+                _viewModel.stringResult = _viewModel.calculator.result;
+                //MessageBox.Show("Resultado: " + _viewModel.calculator.result + "\nDisplay: " + _viewModel.calculator.displayContent);
+                _viewModel.calculator.hasCalculate = true;
+                _viewModel.AddHistory(_viewModel.stringResult, _viewModel.displayContent);
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+
+        }
+    }
+}
