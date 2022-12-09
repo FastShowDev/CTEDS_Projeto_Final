7ff668583548bfe0223fcc9ef66a4c6620262289
FastShowDev <zlFastShow@hotmail.com>
2022-12-09 20:15:29 -0300

    Create Percentage Command

---
+++ Calculadora/Commands/PercentageCommand.cs
@@ -0,0 +1,25 @@
+﻿using Calculadora.Stores;
+using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class PercentageCommand : BaseCommand
+    {
+        private readonly StandardCalculatorViewModel _viewModel;
+        public PercentageCommand(StandardCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            _viewModel.calculator.InsertPercentageInDisplay();
+            _viewModel.UpdateDisplay();
+        }
+    }
+}
