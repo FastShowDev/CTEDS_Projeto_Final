b000f4982ac977c7dc8277f36b9bf07150a58355
FastShowDev <zlFastShow@hotmail.com>
2022-12-13 20:02:40 -0300

    Create PowerBase10 Command

---
+++ Calculadora/Commands/PowerBase10Command.cs
@@ -0,0 +1,31 @@
+﻿using Calculadora.Models;
+using Calculadora.ViewModels;
+using System;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class PowerBase10Command : BaseCommand
+    {
+        private readonly ScientificCalculatorViewModel _viewModel;
+        public PowerBase10Command(ScientificCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            if (parameter != null)
+            {
+                string expression = ((TextBox)parameter).Text;
+                Calculator.ExecutePowerBase10(expression);
+                _viewModel.UpdateDisplay();
+                _viewModel.AddHistory(_viewModel.stringResult, _viewModel.displayContent);
+            }
+            else
+            {
+                throw new NullReferenceException();
+
+            }
+        }
+    }
+}
