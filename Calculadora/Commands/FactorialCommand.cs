20a6d9056bed55933a20d5286e1aeaad7182e865
FastShowDev <zlFastShow@hotmail.com>
2022-12-13 20:02:22 -0300

    Create Factorial Command

---
+++ Calculadora/Commands/FactorialCommand.cs
@@ -0,0 +1,30 @@
+﻿using Calculadora.Models;
+using Calculadora.ViewModels;
+using System;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands
+{
+    public class FactorialCommand : BaseCommand
+    {
+        private readonly ScientificCalculatorViewModel _viewModel;
+        public FactorialCommand(ScientificCalculatorViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public override void Execute(object? parameter)
+        {
+            if(parameter != null)
+            {
+                string expression = ((TextBox)parameter).Text;
+                Calculator.ExecuteFactorial(expression);
+                _viewModel.UpdateDisplay();
+                _viewModel.AddHistory(_viewModel.stringResult, _viewModel.displayContent);
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+    }
+}
