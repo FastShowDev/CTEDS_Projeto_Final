a9ae9f4dc76a65c9cceaff509bbb9ba614aabe42
FastShowDev <zlFastShow@hotmail.com>
2022-12-13 01:26:52 -0300

    Add comments for Calculator Methods

--- Calculadora/Models/Calculator.cs
+++ Calculadora/Models/Calculator.cs
@@ -6,12 +6,18 @@ namespace Calculadora.Models
 {
     public static class Calculator
     {
+        /// <summary>
+        /// Constantes mamáticos e separadores de ponto flutuante
+        /// </summary>
         #region CONSTANTS
         private const string DECIMAL_SPERATOR = ",";
         private const double PI = Math.PI;
         private const double E = Math.E;
         #endregion
 
+        /// <summary>
+        /// Constantes com os símbolos que são mostrados no display.
+        /// </summary>
         #region FUNCTIONS SYMBOLS
         private const string SQUARE_ROOT_SYMBOL = "sqrt";
         private const string SQUARE_SYMBOL = "sqr";
@@ -22,6 +28,11 @@ namespace Calculadora.Models
         #endregion
 
 
+        /// <summary>
+        /// Método que recebe uma expressão matemática simples, realiza seu cálculo utilizando a Engine da Calculadora
+        /// e atualiza o display da calculadora (conteúdo e resultado).
+        /// </summary>
+        /// <param name="expression">Expressão matemática simples</param>
         public static void ExecuteCalculate(string expression)
         {
             CalculatorDisplay.result = expression;
@@ -30,6 +41,12 @@ namespace Calculadora.Models
             return;
         }
 
+
+        /// <summary>
+        /// Método que executa a funcionalidade de elevar ao quadrado. Recebe uma expressão, calcula seu resultado usando a Engine e em seguida
+        /// eleva ao quadrado o resultado obtido e por fim atualiza o display da calculadora.
+        /// </summary>
+        /// <param name="expression">Expressão que será calculada e elevada ao quadrado</param>
         public static void ExecuteSquare(string expression)
         {
             CalculatorDisplay.displayContent = CalculatorEngine.Square(expression).ToString();
@@ -38,6 +55,13 @@ namespace Calculadora.Models
             return;
         }
 
+
+        /// <summary>
+        /// Método que executa a funcionalidade de raiz quadrada. Recebe uma expressão, calcula seu resultado usando a Engine e em seguida
+        /// calcula  a raiz do resultado obtido e por fim atualiza o display da calculadora. Em caso de uma expressão inválida a própria Engine
+        /// faz o tratamento de excessão.
+        /// </summary>
+        /// <param name="expression">Expressão que será calculada e depois tirada a raiz</param>
         public static void ExecuteSquareRoot(string expression)
         {
             CalculatorDisplay.displayContent = CalculatorEngine.SquareRoot(expression).ToString();
@@ -47,6 +71,12 @@ namespace Calculadora.Models
         }
 
 
+        /// <summary>
+        /// Método que executa a funcionalidade de logaritmo de base 10. Recebe uma expressão, calcula seu resultado usando a Engine e em seguida
+        /// calcula o log10 do resultado obtido e por fim atualiza o display da calculadora. Em caso de uma expressão inválida a Engine faz o
+        /// tratamento da excessão.
+        /// </summary>
+        /// <param name="expression">Expressão que será calculado o log10</param>
         public static void ExecuteLog10(string expression)
         {
             CalculatorDisplay.displayContent = CalculatorEngine.Log10(expression).ToString();
@@ -56,6 +86,12 @@ namespace Calculadora.Models
         }
 
 
+        /// <summary>
+        /// Método que executa a funcionalidade de logaritmo neperiano. Recebe uma expressão, calcula seu resultado usando a Engine e em seguida
+        /// calcula o ln do resultado obtido e por fim atualiza o display da calculadora. Em caso de uma expressão inválida a Engine faz o
+        /// tratamento da excessão.
+        /// </summary>
+        /// <param name="expression">Expressão que será calculado o ln</param>
         public static void ExecuteLn(string expression)
         {
             CalculatorDisplay.displayContent = CalculatorEngine.Ln(expression).ToString();
@@ -65,6 +101,12 @@ namespace Calculadora.Models
         }
 
 
+        /// <summary>
+        /// Método que executa a funcionalidade de inversão de número. Recebe uma expressão, calcula seu resultado usando a Engine e em seguida
+        /// calcula o inverso do número obtido e por fim atualiza o display da calculadora. Em caso de uma expressão inválida a Engine faz o
+        /// tratamento da excessão.
+        /// </summary>
+        /// <param name="expression">Expressão que será calculado o inverso</param>
         public static void ExecuteInversion(string expression)
         {
             CalculatorDisplay.displayContent = CalculatorEngine.Inversion(expression).ToString();
@@ -74,6 +116,11 @@ namespace Calculadora.Models
         }
 
 
+        /// <summary>
+        /// Método que executa a funcionalidade de módulo. Recebe uma expressão, calcula seu resultado usando a Engine e em seguida
+        /// calcula o módulo do resultado obtido e por fim atualiza o display da calculadora.
+        /// </summary>
+        /// <param name="expression"></param>
         public static void ExecuteAbsolute(string expression)
         {
             CalculatorDisplay.displayContent = CalculatorEngine.Absolute(expression).ToString();
