b301c791265d71f865d6bdd8fe015c0696a818f1
FastShowDev <zlFastShow@hotmail.com>
2023-05-23 19:12:36 -0300

    Create the StatisticItem model that represents a value in datagrid

---
+++ Calculadora/Models/StatisticItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculadora.Models
+{
+    public class StatisticItem
+    {
+        public int id { get; set; }
+        public double value { get; set; }
+        public double dispersion { get; set; }
+        public double standardDeviation { get; set; }
+
+        public StatisticItem(int id, double value, double dispersion)
+        {
+            this.id = id;
+            this.value = value;
+            this.dispersion = dispersion;
+            standardDeviation = 0;
+        }
+
+    }
+}
