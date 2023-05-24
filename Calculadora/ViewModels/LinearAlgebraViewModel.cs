355c370fdfce4007a6b1ecf158409f13291ec397
FastShowDev <zlFastShow@hotmail.com>
2023-05-24 17:15:46 -0300

    Create initial linear algebra view model

---
+++ Calculadora/ViewModels/LinearAlgebraViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.ViewModels
+{
+    public class LinearAlgebraViewModel : BaseViewModel
+    {
+        public LinearAlgebraViewModel()
+        {
+            ViewName = "Algebra Linear";
+            ViewHeight = 800;
+            ViewWidth = 800;
+        }
+    }
+}
