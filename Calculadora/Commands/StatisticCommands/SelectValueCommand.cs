ec80b3041be9be4216df92f916360e531609afa3
FastShowDev <zlFastShow@hotmail.com>
2023-05-23 23:38:06 -0300

    Create SelectValueCommand

---
+++ Calculadora/Commands/StatisticCommands/SelectValueCommand.cs
@@ -0,0 +1,25 @@
+﻿using Calculadora.Models;
+using Calculadora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Calculadora.Commands.StatisticCommands
+{
+    public class SelectValueCommand : BaseCommand
+    {
+        StatisticItem item;
+        public SelectValueCommand(StatisticItem item)
+        {
+            this.item = item;
+        }
+        public override void Execute(object? parameter)
+        {
+            CheckBox checkBox = parameter as CheckBox;
+            item.selected = (bool) checkBox.IsChecked;
+        }
+    }
+}
