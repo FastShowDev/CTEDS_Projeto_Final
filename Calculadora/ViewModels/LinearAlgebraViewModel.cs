c20f73c3df013533a72c8507dfbe1339ecb7cfdb
FastShowDev <zlFastShow@hotmail.com>
2023-05-24 19:32:15 -0300

    Add new buttons to main menu

--- Calculadora/ViewModels/LinearAlgebraViewModel.cs
+++ Calculadora/ViewModels/LinearAlgebraViewModel.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System;
+
 
 namespace Calculadora.ViewModels
 {
-    public class LinearAlgebraViewModel : BaseViewModel
+    public class LinearAlgebraViewModel : BaseViewModel, INotifyPropertyChanged
     {
         public LinearAlgebraViewModel()
         {
-            ViewName = "Algebra Linear";
+            ViewName = "Álgebra Linear";
             ViewHeight = 800;
             ViewWidth = 800;
         }
---
+++ Calculadora/ViewModels/MatricesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.ViewModels
+{
+    public class MatricesViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        public MatricesViewModel()
+        {
+            ViewName = "Matrizes";
+            ViewHeight = 900;
+            ViewWidth = 900;
+        }
+    }
+}
